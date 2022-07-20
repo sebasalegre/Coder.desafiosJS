c1f0e9d08614b3167602e3681537c0a992486b7e
sebastian alegre <sebastian.alegre96@hotmail.com>
2022-07-19 22:04:06 -0300

    desafio

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 
-class Libro{
-    contructor(titulo, paginas, genero, color ) {
-        this.title = titulo;
-        this.page = paginas;
-        this.genere = genero;
-        this.color = color;
-    }
-    leer() { 
-    console.log("Este libro es del genero " + this.genere) + " y tiene " + (this.pages);
+
+
+while(true){
+  var precioCosto = parseFloat(prompt("Ingrese el precio de costo del producto"));
+
+  if(!isNaN(precioCosto) && precioCosto != null && precioCosto != ""){
+    //alert(es numero)
+    break;
+  } else{ 
+    alert("no es un numero");
+    continue;
   }
 }
- let libro1 = new Libro("IT", 237, "Terror", "Rojo");
- libro1.leer();
- let libro2 = new Libro("La noche del demino", 587, "Suspenso", "Negro");
- libro2.leer(); 
+precioCostoM = precioCosto.toFixed(2);
+alert("el precio de costo del prodcuto a calcular es: "+ precioCostoM);
+console.log("el precio de costo del producto a calcular es: "+ precioCostoM);
+
+
+function CalcularIva(preciocosto) {
+  return (precioCosto * 0.21)
+  
+}
+
+var iva = CalcularIva(precioCosto);
+ivaM= iva.toFixed(2)
+
+alert("el iva del producto ingresado es " + ivaM);
+console.log("el iva del producto ingresado es " + ivaM)
+
+function SumarIva(precioCosto){ 
+  return (precioCosto + (precioCosto * 0.21));
+}
